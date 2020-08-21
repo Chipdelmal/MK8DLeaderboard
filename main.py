296f87f81f9e7699bc787c87bd8be7b83e6ab1c0
chipdelmal <chipdelmal@gmail.com>
2020-08-21 12:26:38 -0700

    Timedelta object parsed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 
+import re
+from datetime import timedelta
 from selenium import webdriver
 # from selenium.webdriver import ActionChains
 # from selenium.webdriver.common.keys import Keys
@@ -6,10 +8,10 @@ from selenium import webdriver
 
 (DRV, OUT) = ('./chromedriver/chromedriver', './out/')
 (SPD, ITM) = ('200cc', 'NoItems')
-# Load driver and mainpage
+# Load driver and mainpage ----------------------------------------------------
 driver = webdriver.Chrome(DRV)
 driver.get('https://www.speedrun.com/mk8dx')
-# Setup dictionaries for buttons
+# Setup dictionaries for buttons ----------------------------------------------
 catItems = {
         'Items': '//*[@id="varnav23164"]/label[1]',
         'NoItems': '//*[@id="varnav23164"]/label[2]'
@@ -26,11 +28,17 @@ tblRow = '//*[@id="leaderboarddiv"]/table/tbody/tr[{}]'
     )
 catBtn.click()
 spdBtn.click()
-# Get table
+# Get table -------------------------------------------------------------------
 table = driver.find_elements_by_tag_name('table')[0]
 
+# Iterate through table rows --------------------------------------------------
 rowIx = 2
+
 row = table.find_elements_by_xpath(tblRow.format(rowIx))[0]
-row.text
-tst = tblRow.format(rowIx) + '/td[1]'
-table.find_elements_by_xpath(tst)[0].text
+entryXPth = [tblRow.format(rowIx)+'/td[{}]'.format(i) for i in range(1, 6)]
+(place, name, time, version, date) = (
+        driver.find_elements_by_xpath(el)[0].text for el in entryXPth
+    )
+# row.text
+(h, m, s, ms) = [int(re.sub("[^0-9]", "", t)) for t in time.split()]
+str(timedelta(hours=h, minutes=m, seconds=s, milliseconds=ms))

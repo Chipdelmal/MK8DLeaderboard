138e2fce8fd4a85c6f16837c60dba6dd72311d18
chipdelmal <chipdelmal@gmail.com>
2020-08-25 11:24:47 -0700

    Separating the scraper

--- scrape.py
+++ scrape.py
@@ -1,20 +1,22 @@
 
+import sys
 import time
 import dateparser
-import seaborn as sns
 import pandas as pd
 import functions as fun
 import constants as const
 from selenium import webdriver
-import matplotlib.pyplot as plt
 
 
+(SPD, ITM) = (sys.argv[1], sys.argv[2])
 (DRV, OUT) = ('./chromedriver/chromedriver_macos', './out/')
-(SPD, ITM) = ('200cc', 'NoItems')
+# (SPD, ITM) = ('200cc', 'NoItems')
 # Load driver and mainpage ----------------------------------------------------
+print('* Loading selenium scraper...')
 driver = webdriver.Chrome(DRV)
 driver.get(const.mainpage)
 # Setup dictionaries for buttons ----------------------------------------------
+print('* Selecting leaderboard...')
 (catBtn, spdBtn) = (
         driver.find_elements_by_xpath(const.catSpeed.get(SPD))[0],
         driver.find_elements_by_xpath(const.catItems.get(ITM))[0]
@@ -23,6 +25,7 @@ catBtn.click()
 spdBtn.click()
 time.sleep(5)
 # Get table -------------------------------------------------------------------
+print('* Parsing leaderboard table...')
 table = driver.find_elements_by_tag_name('table')[0]
 rowNum = len(table.find_elements_by_tag_name('tr'))
 # Iterate through table rows --------------------------------------------------
@@ -47,37 +50,8 @@ for (rix, rank) in enumerate(range(1, rowNum)):
             }, ignore_index=True
         )
 ldBrd = ldBrd.set_index('Rank')
+# Export CSV ------------------------------------------------------------------
+print('* Exporting CSV...')
 ldBrd.to_csv('{}{}-{}.csv'.format(OUT, SPD, ITM))
-# Post process dataframe ------------------------------------------------------
-bins = round(rowNum / 2)
-fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, sharex=True)
-ax2 = sns.violinplot(
-        x="Time", y='Version', hue="Version",
-        data=ldBrd, palette='seismic', split=True, ax=ax2,
-        cut=0
-        # scale="count", inner="stick"
-    )
-ax1 = sns.distplot(
-        ldBrd[ldBrd['Version'] == 'Digital'].get('Time'),
-        bins=bins, kde=False, color="b", ax=ax1
-    )
-ax3 = sns.distplot(
-        ldBrd[ldBrd['Version'] == 'Cartridge'].get('Time'),
-        bins=bins, kde=False, color="r", ax=ax3
-    )
-for p in ax3.patches:  # turn the histogram upside down
-    p.set_height(-p.get_height())
-ax1.set_ylim(0, 7.5)
-ax3.set_ylim(-7.5, 0)
-ax2.legend_.remove()
-
-# Scatter
-diffs = []
-for dt in ldBrd['Date']:
-    today = dateparser.parse('today')
-    delta = today - dt
-    days = delta.days
-    diffs.append(days)
-
-ax4 = plt.scatter(ldBrd['Time'], y=diffs, alpha=0.5)
-plt.yscale('log')
+# Done ------------------------------------------------------------------------
+print('* Done!')

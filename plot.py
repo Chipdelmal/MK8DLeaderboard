77634141ceed6d752f88ee08d819b3d205a97591
chipdelmal <chipdelmal@gmail.com>
2020-08-29 19:35:16 -0700

    Added histograms on top and bottom

--- plot.py
+++ plot.py
@@ -1,80 +1,69 @@
 
-import time
-import dateparser
-import seaborn as sns
 import pandas as pd
-import functions as fun
-import constants as const
+import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
 
 
 (IN, FILE, OUT) = ('./out/', '2020-08-29_200cc_NoItems.csv', './img/')
 # Read dataframe --------------------------------------------------------------
 ldBrd = pd.read_csv(IN+FILE)
-# Preprocess ------------------------------------------------------------------
-rowNum = ldBrd.shape[0]
-bins = round(rowNum / 2)
+colors = ['#1344b8', '#ed174b']
+palette = sns.set_palette(colors)
 # #############################################################################
-# Draft
-# #############################################################################
-fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, sharex=True)
-ax2 = sns.violinplot(
-        x="Time", y='Version', hue="Version",
-        data=ldBrd, palette='seismic', split=True, ax=ax2,
-        cut=0
-        # scale="count", inner="stick"
-    )
-ax1 = sns.distplot(
-        ldBrd[ldBrd['Version'] == 'Digital'].get('Time'),
-        bins=bins, kde=False, color="b", ax=ax1
-    )
-ax3 = sns.distplot(
-        ldBrd[ldBrd['Version'] == 'Cartridge'].get('Time'),
-        bins=bins, kde=False, color="r", ax=ax3
-    )
-for p in ax3.patches:  # turn the histogram upside down
-    p.set_height(-p.get_height())
-ax1.set_ylim(0, 7.5)
-ax3.set_ylim(-7.5, 0)
-ax2.legend_.remove()
-
-# Scatter
-diffs = []
-for dt in ldBrd['Date']:
-    today = dateparser.parse('today')
-    delta = today - dt
-    days = delta.days
-    diffs.append(days)
-
-ax4 = plt.scatter(ldBrd['Time'], y=diffs, alpha=0.5)
-plt.yscale('log')
-# #############################################################################
-# Draft
+# Plots
 # #############################################################################
 (dFlt, cFlt) = (ldBrd['Version'] == 'Digital', ldBrd['Version'] == 'Cartridge')
 (dTimes, cTimes) = (ldBrd[dFlt]['Time'], ldBrd[cFlt]['Time'])
-# Colors ----------------------------------------------------------------------
-colors = ['#ed174b', '#1344b8']
-palette = sns.set_palette(colors)
-# Plot ------------------------------------------------------------------------
-(fig, ax) = plt.subplots(1, 1, sharex=True, figsize=(10, 2))
+# (fig, (axt, ax, axb)) = plt.subplots(3, 1, sharex=True, figsize=(6, 2))
+fig = plt.figure(figsize=(18, 8))
+spec = gridspec.GridSpec(3, 1, height_ratios=[.5, 1, .5], hspace=0)
+axb = fig.add_subplot(spec[0])
+ax = fig.add_subplot(spec[1])
+axt = fig.add_subplot(spec[2])
+# Violin plot #################################################################
 ax = sns.violinplot(
         y=["Attempts"]*len(ldBrd), x='Time', hue="Version",
         data=ldBrd, palette=palette, split=True, ax=ax,
-        cut=0, orient="h", saturation=.75
+        cut=0, orient="h", saturation=.75, inner=None, linewidth=.1
     )
 # Post Plot -------------------------------------------------------------------
 handles, _ = ax.get_legend_handles_labels()
 (minT, maxT) = (min(list(ldBrd["Time"])), max(list(ldBrd["Time"])))
-pRange = (minT-minT*.01, maxT+maxT*.01)
+(pRange, height) = ((minT-minT*.01, maxT+maxT*.01), .5)
 ax.legend(handles, ["D", "C"])
 ax.hlines(
         [0], xmin=pRange[0], xmax=pRange[1],
-        colors='k', linewidth=.75, ls='dashed'
+        colors='k', linewidth=.5, ls='dashed'
     )
-ax.vlines(dTimes, ymin=-1, ymax=0, colors=colors[0], linewidth=.2)
-ax.vlines(cTimes, ymin=0, ymax=1, colors=colors[1], linewidth=.2)
-# Save ------------------------------------------------------------------------
+ax.vlines(dTimes, ymin=-height, ymax=0, colors=colors[0], linewidth=.2)
+ax.vlines(cTimes, ymin=0, ymax=height, colors=colors[1], linewidth=.2)
+for violin, alpha in zip(ax.collections[:2], [.2, .2]):
+    violin.set_alpha(alpha)
+ax.set_ylim(-height, height)
+# Labels and limits  ----------------------------------------------------------
 ax.set_xlim(*pRange)
 ax.legend_.remove()
-fig.savefig(OUT+'violin.png', dpi=500)
+ax.set_yticklabels('')
+# Histograms ##################################################################
+rowNum = ldBrd.shape[0]
+bins = round(rowNum / 2)
+hi = 8
+axt = sns.distplot(
+        ldBrd[dFlt].get('Time'),
+        bins=bins, kde=False, color=colors[0], ax=axt
+    )
+axb = sns.distplot(
+        ldBrd[cFlt].get('Time'),
+        bins=bins, kde=False, color=colors[1], ax=axb
+    )
+for p in axt.patches:
+    p.set_height(-p.get_height())
+axb.set_ylim(0, hi)
+axt.set_ylim(-hi, 0)
+axt.set_xlim(*pRange)
+axb.set_xlim(*pRange)
+# #############################################################################
+# Save
+# #############################################################################
+fig.savefig(OUT+'violin.png', dpi=500, bbox='tight', pad_inches=.1)

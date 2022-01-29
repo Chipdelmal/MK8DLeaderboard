db063f3be81910cc2f09624aef29e5ef280ceab3
chipdelmal <chipdelmal@gmail.com>
2022-01-29 14:31:53 -0800

    Leaderboard history links are parsed now

--- leaderboard.py
+++ leaderboard.py
@@ -1,16 +1,17 @@
 
 import sys
+import csv
 import time
-import dateparser
 import pandas as pd
-import functions as fun
 import constants as const
+from os import path
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 
 
 # (TRK, SPD, ITM) = (sys.argv[1], sys.argv[2], sys.argv[3])
+BASE_URL = 'https://www.speedrun.com'
 (DRV, OUT) = (
     '/home/chipdelmal/Documents/GitHub/MK8DLeaderboard/chromedriver/chromedriver',
     '/home/chipdelmal/Documents/MK8D/Leaderboard/'
@@ -52,14 +53,18 @@ shwn = '//*[@id="leaderboard-menu"]/div/div[2]/div[1]/ul/li[4]/ul/a[2]'
 shwnBtn = driver.find_elements_by_xpath(shwn)[0]
 shwnBtn.click()
 ###############################################################################
-# Get table rows
+# Get table rows (links to runs)
 ###############################################################################
 xPath = '//*[@id="primary-leaderboard"]/tbody/tr[{}]'
-rows = 600 # len(driver.find_elements_by_tag_name('tr'))
+rows = 604 # len(driver.find_elements_by_tag_name('tr'))
 runLinks = []
 for row in range(1, rows):
     r = xPath.format(row)
     rObj = driver.find_element_by_xpath(r)
     runLink = rObj.get_attribute('data-target')
-    runLinks.append(runLink)
-runLinks
+    runLinks.append(BASE_URL+runLink)
+###############################################################################
+# Export list to file
+###############################################################################
+runLinksDF = pd.DataFrame(runLinks, columns=['Link'])
+runLinksDF.to_csv(path.join(OUT, 'leadHistory.csv'), index=False, header=False)

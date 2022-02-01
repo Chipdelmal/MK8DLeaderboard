4eb754fab8a2e0b7c95923d4ac42261cf280aa75
chipdelmal <chipdelmal@gmail.com>
2022-01-31 19:42:02 -0800

    Minor updates to plots

--- leaderboardAnalysis.py
+++ leaderboardAnalysis.py
@@ -2,6 +2,7 @@
 from os import path
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 FNAME = 'leadRunsHistory_test.csv'
 OUT = '/home/chipdelmal/Documents/MK8D/Leaderboard/'
@@ -31,6 +32,23 @@ dfSub = df[df['Date']<=dte]
 ###############################################################################
 # Plots
 ###############################################################################
+highName = 'chipdelmal'
+(fg, bg, aspect) = ('#3a0ca322', '#f7258599', .3)
+tScale = (1e9*60)
+# Plot
+(fig, ax) = plt.subplots()
 for name in namesUQ:
     dfSub = df[df['Runner']==name]
-    plt.plot(dfSub['Time'], dfSub['Date'])
+    if name!='chipdelmal':
+        ax.plot(dfSub['Date'], dfSub['Time']/tScale, color=fg)
+        ax.plot(dfSub['Date'], dfSub['Time']/tScale, color=fg, marker='.')
+dfSub = df[df['Runner']==highName]
+ax.plot(dfSub['Date'], dfSub['Time']/tScale, color=bg)
+ax.plot(dfSub['Date'], dfSub['Time']/tScale, color=bg, marker='.')
+# Styling
+ax.set_aspect(aspect/ax.get_data_ratio())
+###############################################################################
+# Debugging
+###############################################################################
+minT = min(df['Time'])
+df[df['Time']==minT]
